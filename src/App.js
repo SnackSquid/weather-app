affe1e679b41c6f1e3ac0835413d0eb90b51767c
Spencer Williams <spilliams@gmail.com>
2022-08-31 21:57:13 -0500

    Set up API call and state container

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [weater, updateWeather] = useState({
+    temp: 0,
+    tempMax: 0,
+    tempMin: 0,
+    feelsLike: 0,
+    location: '',
+  });
+
+  const setWeather = (props) => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p></p>
     </div>
   );
 }
+
+async function getWeather() {
+  const cityName = 'Chicago';
+  const apiKey = '3a0e7e455c0a21e0b314e046ad8e27d0';
+  const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+  const response = await fetch(api, {mode: 'cors'});
+  const weather = await response.json();
+  console.log(weather)
+}
+
+getWeather();
 
 export default App;
