a0935ec7fc08d963d2849069873563a061fa8855
Spencer Williams <Spilliams@gmail.com>
2022-09-02 09:09:43 -0500

    Fix celsius to fahrenheit async issue

--- src/App.js
+++ src/App.js
@@ -8,10 +8,49 @@ function App() {
     tempMax: 0,
     tempMin: 0,
     feelsLike: 0,
+    humidity: 0,
+    sky: '',
     location: "Chicago",
   });
 
-  const getWeather = async() => {
+  const [celsiusOrFahrenheit, updateCelsiusOrFahrenheit] = useState(false);
+  const [degreeMarker, updateDegreeMarker] = useState("F");
+
+  // make an API call using the default location (Chicago, the best city on Earth)
+  useEffect(() => {
+    getWeather();
+  }, [celsiusOrFahrenheit])
+
+  // changes the temp measurement from Celsiuse (true) and Fahrenheit (false)
+  // then uses another API call to get the updated weather data
+  const changeTempMeasurement = () => {
+    if (celsiusOrFahrenheit) {
+      const change = false;
+      updateCelsiusOrFahrenheit(change);
+    } else {
+      const change = true;
+      updateCelsiusOrFahrenheit(change);
+    }
+  };
+
+  // converts the API called data from Kelvin to the current selected measurement
+  const convertFromKelvin = (temp) => {
+    const kelvin = 273.15;
+    if (celsiusOrFahrenheit) {
+      const newTemp = Math.round(temp - kelvin);
+      const celsius = "C";
+      updateDegreeMarker(celsius);
+      return newTemp;
+    } else {
+      const newTemp = Math.round((temp - kelvin) * 1.8 + 32);
+      const fahrenheit = "F";
+      updateDegreeMarker(fahrenheit);
+      return newTemp;
+    }
+  };
+
+  // makes the API call and sets the data into the state variables
+  const getWeather = async () => {
     const cityName = weather.location;
     const apiKey = "3a0e7e455c0a21e0b314e046ad8e27d0";
     const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
@@ -19,71 +58,59 @@ function App() {
     try {
       const response = await fetch(api, { mode: "cors" });
       const weather = await response.json();
+      console.log(weather);
       const returnData = {
-        temp: weather.main.temp,
-        tempMax: weather.main.temp_max,
-        tempMin: weather.main.temp_min,
-        feelsLike: weather.main.feels_like,
-        location: weather.name}
+        temp: convertFromKelvin(weather.main.temp),
+        tempMax: convertFromKelvin(weather.main.temp_max),
+        tempMin: convertFromKelvin(weather.main.temp_min),
+        feelsLike: convertFromKelvin(weather.main.feels_like),
+        humidity: weather.main.humidity,
+        sky: weather.weather[0].main,
+        location: weather.name,
+      };
 
       updateWeather(returnData);
-      return returnData;
-      
     } catch (error) {
       alert(error);
     }
-    
-  }
+  };
 
-  const setNewWeather = (props) => {
-    const city = {location: props};
+  // gets the value of the city from the input bar to use for the API call
+  const setNewCity = (props) => {
+    const city = { location: props };
     const newCityArray = Object.assign(weather, city);
     updateWeather(newCityArray);
     getWeather();
-
-  }
-
-  useEffect(() => {
-    <WeatherDisplay main={weather} />
-  }, [weather])
-
-
+  };
 
   return (
     <div className="main">
-      <Header />
-      <WeatherDisplay 
-      main={weather} />
-      <WeatherSearch
-      onSearch={getWeather}
-      handleChange={setNewWeather} />
+      <Header
+        degree={celsiusOrFahrenheit}
+        changeTempMeasurement={changeTempMeasurement}
+      />
+      <WeatherDisplay main={weather} degree={degreeMarker} />
+      <WeatherSearch onSearch={getWeather} handleChange={setNewCity} />
     </div>
   );
 }
 
+// enable the search bar
 function WeatherSearch(props) {
-
   const handleTextInput = (event) => {
     event.preventDefault();
     props.handleChange(event.target[0].value);
-  }
+  };
+
   return (
-    <div>
+    <div className="searchBar">
       <form onSubmit={handleTextInput}>
         <label>Search for weather:</label>
-        <input type="text" 
-        id="search"
-        value={props.location}
-        ></input>
-        <button type="submit"
-        >Search</button>
+        <input type="text" id="search" value={props.location}></input>
+        <button type="submit">Search</button>
       </form>
     </div>
   );
 }
-
-
-
-
 
 export default App;
--- src/components/SiteBuilder.js
+++ src/components/SiteBuilder.js
@@ -1,21 +1,37 @@
 
+function Header(props) {
 
-function Header() {
+  const getMarker = () => {
+    return (props.degree ? 'Fahrenheit' : 'Celsius')
+  }
+
+  const changeTemp = (event) => {
+    event.preventDefault();
+    props.changeTempMeasurement(event);
+  }
+  
   return (
     <div className="header">
       <h1>Weather.me.Weather</h1>
+      <button onClick={changeTemp}
+      value={props.degree}>Change to {getMarker(props.degree)}</button>
     </div>
   );
 }
 
 function WeatherDisplay(props) {
   const weather = props.main;
+  const marker = props.degree;
   return (
     <div className="weatherDisplay">
-      <p>Temp: {weather.temp}</p>
-      <p>High: {weather.tempMax}</p>
-      <p>Low: {weather.tempMin}</p>
-      <p>Location: {weather.location}</p>
+      <h2>Weather for {weather.location}</h2>
+      <p>Current temperature: {weather.temp} °{marker}</p>
+      <p>Humidity: {weather.humidity}%</p>
+      <p>Today's high: {weather.tempMax} °{marker}</p>
+      <p>Today's low: {weather.tempMin} °{marker}</p>
+      <p>Real feel: {weather.feelsLike} °{marker}</p>
+      <p>Sky conditions: {weather.sky}</p>
+      
     </div>
   );
 }
